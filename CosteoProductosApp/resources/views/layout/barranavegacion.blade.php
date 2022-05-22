55a8e052b4f935158f9eae2818191f05008a09e5
Fran <tc10010@ues.edu.sv>
2022-05-21 18:43:59 -0600

    Plantilla base y primera migracion de BBDD

---
+++ CosteoProductosApp/resources/views/layout/barranavegacion.blade.php
@@ -0,0 +1,63 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Gestion de Pedidos
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Pedidos</a></li>
+                        <li><a class="dropdown-item" href="#">Lotes de Fabricacion</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Servicios y Equipos
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Equipos y enseres</a></li>
+                        <li><a class="dropdown-item" href="#">Servicios</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Insumos y Mercancias
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Productos</a></li>
+                        <li><a class="dropdown-item" href="#">Materia prima</a></li>
+                        <li><a class="dropdown-item" href="#">Recetas</a></li>
+                        <li><a class="dropdown-item" href="#">Unidad de medida</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Proveedores</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Empleados</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Calculo de Costos</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Reportes
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Opcion de Reporte 1</a></li>
+                        <li><a class="dropdown-item" href="#">Opcion de Reporte 2</a></li>
+                        <li><a class="dropdown-item" href="#">Opcion de Reporte 3</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ CosteoProductosApp/resources/views/layout/pie.blade.php
@@ -0,0 +1,14 @@
+<style> 
+    footer{
+        width:100%;
+        min-height:60px;    
+        background-color: #222; /* This color gets inverted color or you can add navbar inverse class in html */
+    }
+</style>
+
+<footer class="footer navbar-fixed-bottom">
+    <div class="container">
+        <h6 class="text-muted lead">&copy Copiright</h6>
+        <h6 class="text-muted">Alfajores de la Mami</h6>
+    </div>
+</footer>
