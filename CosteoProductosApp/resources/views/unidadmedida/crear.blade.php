e93f99c7c9768bb293d9781b39391024e46d8fed
herbherra <herberth.herrador@outlook.com>
2022-05-31 05:29:09 -0600

    Formularios
    ajustes a los formularios, crear y ver de unidad de medida

--- CosteoProductosApp/resources/views/unidadmedida/crear.blade.php
+++ CosteoProductosApp/resources/views/unidadmedida/crear.blade.php
@@ -3,6 +3,7 @@
 @section("contenido")
 
 <div class="row"style="margin:auto;margin-bottom: 5x;">
+
             <div class="col-md-12" style="margin:auto;background: transparent;">
                 <div class="pull-right" style="background:transparent;">
                     <a class="btn btn-success" data-toggle="tooltip" data-placement="top" title="Regresar" href="{{Route('ver_unidad_medida')}}" style="margin-top: 10px;margin-bottom: 10px;"> 
@@ -27,6 +28,7 @@
         @csrf
 
         <div class="form-group row">
+            <div class="row" style="margin: auto;"><br></div>
             <label for="inputText" class="col-sm-2 col-form-label">Nombre:</label>
             <div class="col-sm-10">
                 <input type="text"  name="nombre" size="20" placeholder="Nombre" value="{{old('nombre')}}" style="background:white">
@@ -34,8 +36,7 @@
             @error('nombre')
                 <small class="text-danger">{{$message}}</small>
             @enderror
-        </div>
-        <div class="form-group row" style="margin: auto;" >
+        
             <label for="inputSelect" class="col-sm-2 col-form-label">Magnitud:</label>
             <div class="col-sm-10">
                 @if(count($magnitud))
@@ -48,22 +49,24 @@
                     <select name="nombre_magnitud" aria-label="Default select example" column="70"></select>
                 @endif
             </div>
-        </div>
-        <div class="form-group row" style="margin: auto;">
+        
             <label for="inputText" class="col-sm-2 col-form-label">Simbolo:</label>
             <div class="col-sm-10">
                 <input type="text"  name="simbolo" size="10" placeholder="Simbolo" value="{{old('simbolo')}}"style="background:white">
             </div>
+            <label><br></label>
             @error('simbolo')
                 <small class="text-danger">{{$message}}</small>
             @enderror
         </div>
+
         <div class="form-group row"style="margin: auto;" >
-            <div class="col-sm-12"> <br>
+            <div class="col-sm-12"> 
                 <button type="submit" class="btn btn-primary" style ="margin: 15px;box-shadow: 1px -1px 10px 1px;float: right;">Agregar</button>
                 <button type="reset" class="btn btn-primary"style ="margin: 15px;box-shadow: 1px -1px 10px 1px;float:right">Limpiar</button>
             </div>
         </div>
+
     </form>
     
 </div>
